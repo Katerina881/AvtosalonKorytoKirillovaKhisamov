0be4e2bf95735d3dbe09ad7792e68360273fa690
Katerina881 <mr.alodov@mail.ru>
2020-04-30 18:01:36 +0400

    k

---
+++ KorytoKirillovaKhisamov/KorytoModel/Detail.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace KorytoModel
+{
+    [DataContract]
+    public class Detail
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        [Required]
+        public string DetailName { get; set; }
+
+        [DataMember]
+        [Required]
+        public int TotalAmount { get; set; }
+
+        [DataMember]
+        [Required]
+        public int TotalReserve { get; set; }
+
+        [ForeignKey("DetailId")]
+        public virtual List<CarDetail> CarDetails { get; set; }
+
+        [ForeignKey("DetailId")]
+        public virtual List<DetailRequest> DetailRequests { get; set; }
+    }
+}
